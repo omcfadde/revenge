a3b9e5313e41dd65b262533184774319bd1f7131
Oliver McFadden <z3ro.geek@gmail.com>
2007-05-02 13:49:58 +0000

    Moved the Radeon defines into the main header file.

--- src/main.h
+++ src/main.h
@@ -20,6 +20,24 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#define REG_ADDR 0xe5000000	/* lspci */
+#define REG_SIZE 0x7d4		/* lspci */
+
+#define RADEON_MC_AGP_LOCATION		0x014c
+#define RADEON_CP_RB_BASE		0x0700
+#define RADEON_CP_RB_CNTL		0x0704
+#define RADEON_CP_RB_RPTR		0x0710
+#define RADEON_CP_IB_BASE		0x0738
+#define RADEON_CP_PACKET0		0x00000000
+#define CP_PACKET0( reg, n )						\
+	(RADEON_CP_PACKET0 | ((n) << 16) | ((reg) >> 2))
+
+#define RADEON_CP_PACKET_MASK		0xC0000000
+#define RADEON_CP_PACKET_COUNT_MASK	0x3fff0000
+#define RADEON_CP_PACKET0_REG_MASK	0x000007ff
+#define RADEON_CP_PACKET1_REG0_MASK	0x000007ff
+#define RADEON_CP_PACKET1_REG1_MASK	0x003ff800
+
 extern int option_blob;
 extern int option_ioctl;
 extern int option_verbose;
