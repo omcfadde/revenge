e10f43e17b2a57aeefe2434c7d2d3658afc5f181
Oliver McFadden <z3ro.geek@gmail.com>
2007-06-07 18:49:02 +0000

    Added getopt_long option parsing.

--- src/main.c
+++ src/main.c
@@ -18,29 +18,19 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include <SDL.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include "detect_agp.h"
-#include "main.h"
-#include "ring.h"
-#include "test.h"
-
-#include "config.h"
-
-int option_blob = 0;		/* define to 1 if you're using the blob. */
-int option_ioctl = 0;		/* define to 1 for ioctl dumping. */
-int option_verbose = 0;		/* define to 1 for printing of information that
-				   is not normally meaningful; for example, the
-				   ring head/tail pointers. Information that is
-				   normally meaningful is always printed. */
-
-int option_ioctl_before = 0;	/* define to 1 to dump the (write) ioctls before
-				   executing them as well as after executing
-				   them (the default) see below. */
-int option_per_file = 0;	/* define to 1 for per file stdout redirection. */
+/*
+ * TODO: add functionality to read/write any register (like radeontool), in
+ * addidtion to the normal functionality of running the tests and creating a
+ * dump.
+ *
+ * TODO: add ability to dump MMIO stuff via interfacing with the kernel. the
+ * kernel would be patched with the kmmio patch, and this program will contain
+ * the code to read that. See mmio-trace. This is very important as apparently
+ * (unconfirmed) the newer blobs don't use indirect buffers anymore.
+ *
+ * TODO: make a better testing framework (test.c, test_<type>.c, ...) etc the
+ * test_tri stuff in here is just a hack for testing.
+ */
 
 /*
  * Note about ioctls.
@@ -55,6 +45,29 @@ int option_per_file = 0;	/* define to 1 for per file stdout redirection. */
  * value. This is why it can be useful to dump the ioctl's before and after
  * executing them, but it's not really needed by default.
  */
+
+#include <SDL.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "detect_agp.h"
+#include "main.h"
+#include "ring.h"
+#include "test.h"
+
+#include "config.h"
+
+int option_blob = 0;		/* define to 1 if you're using the blob. */
+int option_ioctl = 0;		/* define to 1 for ioctl dumping. */
+int option_ioctl_before = 0;	/* define to 1 to dump the (write) ioctls
+				   before executing them as well as after
+				   executing them (the default) see below. */
+int option_per_file = 0;	/* define to 1 for per file stdout redirection. */
+int option_verbose = 0;		/* define to 1 for printing of information that
+				   is not normally meaningful; for example, the
+				   ring head/tail pointers. Information that is
+				   normally meaningful is always printed. */
 
 static int
 alloc_opengl (void)
@@ -102,93 +115,36 @@ alloc_opengl (void)
   return 0;
 }
 
-/*
- * This simple method is used to parse commandline options. I might replace this
- * with getopt_long in the future...
- */
-void
-parse_options (int argc, char **argv)
-{
-  int i;
-  char **options;
-
-  options = argv;
-  for (i = 0; i < argc; i++, options++)
-    {
-      char *option = *options;
-      if (*option == '-')
-	{
-	  if ((strcmp ("h", option + 1) == 0)
-	      || (strcmp ("-help", option + 1) == 0))
-	    {
-	      printf ("%s help\n-----\n", PACKAGE_STRING);
-	      printf ("Option                  Short   Information\n");
-	      printf
-		(" --trace-blob                   Trace the blob for information\n");
-	      printf
-		(" --trace-ioctls                 Trace the ioctls access\n");
-	      printf
-		(" --trace-ioctls-before          Trace the ioctls before writing them\n");
-	      printf
-		(" --help                 -h      Print this help screen and exit\n");
-	      printf
-		(" --verbose              -v      Verbose information\n");
-	      printf
-		(" --per-file                     Redirect output to a separate file for each test\n");
-	      exit (0);
-	    }
-	  else if (strcmp ("-trace-blob", option + 1) == 0)
-	    {
-	      printf ("enabling blob tracing\n");
-	      option_blob = 1;
-	    }
-	  else if (strcmp ("-trace-ioctls", option + 1) == 0)
-	    {
-	      printf ("Enabling ioctls tracing\n");
-	      option_ioctl = 1;
-	    }
-	  else if (strcmp ("-trace-ioctls-before", option + 1) == 0)
-	    {
-	      printf ("Dumping ioctls before writing them\n");
-	      option_ioctl_before = 1;
-	    }
-	  else if ((strcmp ("v", option + 1) == 0)
-		   || (strcmp ("-verbose", option + 1) == 0))
-	    {
-	      printf ("Enabling verbose output\n");
-	      option_verbose = 1;
-	    }
-	  else if (strcmp ("-per-file", option + 1) == 0)
-	    {
-	      printf ("Enabling per file redirection\n");
-	      option_per_file = 1;
-	    }
-	  else
-	    {
-	      printf ("Unknown option: %s\n", option);
-	    }
-	}
-    }
-}
-
-/*
- * TODO: add functionality to read/write any register (like radeontool), in
- * addidtion to the normal functionality of running the tests and creating a
- * dump.
- *
- * TODO: add ability to dump MMIO stuff via interfacing with the kernel. the
- * kernel would be patched with the kmmio patch, and this program will contain
- * the code to read that. See mmio-trace. This is very important as apparently
- * (unconfirmed) the newer blobs don't use indirect buffers anymore.
- *
- * TODO: make a better testing framework (test.c, test_<type>.c, ...) etc the
- * test_tri stuff in here is just a hack for testing.
- */
+static struct option long_options[] = {
+  {"blob", no_argument, &option_blob, 1},
+  {"ioctl", no_argument, &option_ioctl, 1},
+  {"ioctl-before", no_argument, &option_ioctl_before, 1},
+  {"per-file", no_argument, &option_per_file, 1},
+  {"verbose", no_argument, &option_verbose, 1},
+  {0, 0, 0, 0},
+};
 
 int
 main (int argc, char **argv)
 {
-  parse_options (argc, argv);
+  int i = 0;
+  int opt;
+
+  while ((opt = getopt_long (argc, argv, "v", long_options, &i)) != -1)
+    {
+      switch (opt)
+	{
+	case 0:
+	  /* empty */
+	  break;
+	case 'v':
+	  option_verbose = 1;
+	  break;
+	default:
+	  abort ();
+	  break;
+	}
+    }
 
   if (alloc_opengl ())
     {
