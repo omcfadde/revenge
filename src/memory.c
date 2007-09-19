dde77a24e2af0c168bdbc0cfcf22ba2a651ec27d
Maciej Cencora <m.cencora@gmail.com>
2007-09-19 21:17:51 +0000

    Corrected unaligned memory access on PCI-E.

--- src/memory.c
+++ src/memory.c
@@ -70,31 +70,35 @@ gart_to_phys (unsigned int addr)
 static void *
 memory_read_pcigart (unsigned int addr, unsigned int size)
 {
-  int i;
+  unsigned int cpa;		//current page address
+  unsigned int lpa;		//last page address
+
+  unsigned int addr_mod, buf_size;
+  void *dst, *tmp, *cbuf;
   void *page_mem_map;
-  void *tmp;
-  unsigned int page_addr, page_phys_addr;
 
-  assert ((addr % ATI_PCIGART_PAGE_SIZE) == 0);
-  size = round_up (size, ATI_PCIGART_PAGE_SIZE);
+  addr_mod = addr % ATI_PCIGART_PAGE_SIZE;
+  buf_size = round_up (addr_mod + size, ATI_PCIGART_PAGE_SIZE);
+  cbuf = tmp = (void *) malloc (buf_size);
 
-  tmp = (void *) malloc (size);
+  cpa = round_down (addr, ATI_PCIGART_PAGE_SIZE);	// first page
+  lpa = cpa + buf_size;		// last page
 
-  for (i = 0; i < size; i += ATI_PCIGART_PAGE_SIZE)
+  // printf ("addr = 0x%08x, size = 0x%08x, first page = 0x%08x, last page = 0x%08x\n", addr, size, cpa, lpa);
+
+  for (; cpa < lpa;
+       cpa += ATI_PCIGART_PAGE_SIZE, tmp += ATI_PCIGART_PAGE_SIZE)
     {
-      page_addr = addr + i;
-      page_phys_addr = gart_to_phys (page_addr);
-
       if ((page_mem_map =
 	   mmap (NULL, ATI_PCIGART_PAGE_SIZE, PROT_READ | PROT_WRITE,
-		 MAP_SHARED, mem_fd, page_phys_addr)) == MAP_FAILED)
+		 MAP_SHARED, mem_fd, gart_to_phys (cpa))) == MAP_FAILED)
 	{
 	  fprintf (stderr, "%s: %s\n", program_invocation_short_name,
 		   strerror (errno));
 	  exit (EXIT_FAILURE);
 	}
 
-      memcpy (tmp + i, page_mem_map, ATI_PCIGART_PAGE_SIZE);
+      memcpy (tmp, page_mem_map, ATI_PCIGART_PAGE_SIZE);
 
       if (munmap (page_mem_map, ATI_PCIGART_PAGE_SIZE) < 0)
 	{
@@ -104,7 +108,11 @@ memory_read_pcigart (unsigned int addr, unsigned int size)
 	}
     }
 
-  return tmp;
+  dst = (void *) malloc (size);
+  memcpy (dst, cbuf + addr_mod, size);
+  free (cbuf);
+
+  return dst;
 }
 
 void *
